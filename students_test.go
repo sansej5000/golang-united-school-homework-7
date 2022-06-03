d0f6c7b89eb3a9d022486c77fc4a1ecc87f98a56
sansej5000 <99507931+sansej5000@users.noreply.github.com>
2022-06-03 11:18:03 +0300

    Update students_test.go

--- students_test.go
+++ students_test.go
@@ -12,7 +12,6 @@ var Alex_Ford Person = Person{firstName: "Alex", lastName: "Ford", birthDay: tim
 
 const error_sort string = "wrong sorting"
 
-// DO NOT EDIT THIS FUNCTION
 func init() {
 	content, err := os.ReadFile("students_test.go")
 	if err != nil {
@@ -70,5 +69,79 @@ func TestLessAllDifferent(t *testing.T) {
 	res := people.Less(0, 1)
 	if !res {
 		t.Errorf(error_sort)
+	}
+}
+
+func TestNewRowsSameLength(t *testing.T) {
+	input := `1 2
+		  	3 4 
+		  	5 6`
+	_, ok := New(input)
+	if ok != nil {
+		t.Errorf("Rows of the same length, expected result: {3 2 [1 2 3 4 5 6]}, <nil>")
+	}
+}
+
+func TestNewRowsDifferentLengths(t *testing.T) {
+	input := `1
+		  	3 4 
+		  	5 6`
+	_, ok := New(input)
+	if ok == nil {
+		t.Errorf("Rows of different lengths, expected result: <nil> Rows need to be the same length")
+	}
+}
+
+func TestNewLetter(t *testing.T) {
+	input := `1 a
+		  	3 4 
+		  	5 6`
+	_, ok := New(input)
+	if ok != nil {
+		t.Errorf("Letter instead of number, expected result: <nil> strconv.Atoi: parsing 'a': invalid syntax")
+	}
+}
+
+func TestRows(t *testing.T) {
+	input := `1 2
+		  	3 4 
+		  	5 6`
+	matrix, _ := New(input)
+	mtrx:=matrix.Rows()
+	if mtrx == nil {
+		t.Errorf("Returned data is not correct, expected result: [[1 2] [3 4] [5 6]]")
+	}
+}
+
+func TestCols(t *testing.T) {
+	input := `1 2
+		  	3 4 
+		  	5 6`
+	matrix, _ := New(input)
+	mtrx:=matrix.Rows()
+	if mtrx == nil {
+		t.Errorf("Returned data is not correct, expected result: [[1 3 5] [2 4 6]]")
+	}
+}
+
+func TestSetCorrectIndex(t *testing.T) {
+	input := `1 2
+		  	3 4 
+		  	5 6`
+	matrix, _ := New(input)
+	mtrx:=matrix.Set(1,1,22)
+	if !mtrx {
+		t.Errorf("Returned data is not correct, expected result: true")
+	}
+}
+
+func TestSetInvalidIndex(t *testing.T) {
+	input := `1 2
+		  	3 4 
+		  	5 6`
+	matrix, _ := New(input)
+	mtrx:=matrix.Set(1,5,22)
+	if mtrx {
+		t.Errorf("Returned data is not correct, expected result: false")
 	}
 }
